ef5603352952c5b692f976a69dbea9bfb0c4dfec
Shibu <shibuthannikkunnath@gmail.com>
2017-12-09 18:06:32 -0500

    Initial create of Tweets aspnet core mvc app.

---
+++ Tweets.Web/Startup.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Tweets.Web
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //Configure MVC in services collection
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            //Allow serving static files such as *.js and *.css
+            app.UseStaticFiles();
+
+            //Add MVC into middleware pipeline
+            app.UseMvcWithDefaultRoute();
+        }
+    }
+}
