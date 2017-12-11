81199917aa3dc312770a2e55c91d2799a41cae83
shibut12 <shibu@hotmail.co.in>
2017-12-10 20:23:51 -0500

    Added tests foe HomeController, enabled Coverage report on codecov.io

---
+++ Tweets.Web.Tests/SutHomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Tweets.Web.Controllers;
+using Xunit;
+
+namespace Tweets.Web.Tests
+{
+    public class SutHomeController
+    {
+        [Fact]
+        public void IndexShouldReturnAView()
+        {
+            var home = new HomeController();
+            var response = home.Index();
+            Assert.IsType<ViewResult>(response);
+        }
+    }
+}
